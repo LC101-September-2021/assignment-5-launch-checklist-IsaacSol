56a07324abc75656cf05142005e5be6412908f79
IsaacSol <soloisap001@gmail.com>
2021-12-06 09:46:25 -0600

    first changes

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // Write your JavaScript code here!
 
+const { myFetch } = require("./scriptHelper");
+const { validateInput } = require("./scriptHelper")
+
 window.addEventListener("load", function() {
+    // let formSubmit = document.getElementById("formSubmit")
+    // let divNum = document.getElementsByClassName("formField")
+    // formSubmit.addEventListener("click", function(){
+    //     let form = document.forms["testForm"]["copilotName"].value
+    //     alert(form)
+    // })
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
@@ -13,4 +22,6 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
+   
+
 });
